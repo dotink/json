91636129762a6b85f729d4945afe671bd1d7f81d
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2020-01-17 15:34:13 -0800

    Prepare array elements with the same nesting level as the array.

--- src/normalizers/_Array.php
+++ src/normalizers/_Array.php
@@ -9,6 +9,16 @@ class _Array extends \Json\Normalizer
 {
 	public function jsonSerialize()
 	{
-		return array_map([static::class, 'prepare'], $this('data'));
+		return array_map(
+			function($item) {
+				//
+				// Prepare the items of the array with our own nesting level (if we're root level,
+				// treat the items as root level).
+				//
+
+				return static::prepare($item, $this('nested'));
+			},
+			$this('data')
+		);
 	}
 }
